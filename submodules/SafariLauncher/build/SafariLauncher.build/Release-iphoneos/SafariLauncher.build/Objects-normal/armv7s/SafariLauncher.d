cd58c0961d617ead374e0ba60fd398a29fb3377e
thomas.li <thomas.li@sinobot.com.cn>
2014-10-20 15:55:52 -0700

    build safariLauncher for apple ID : libiao.2005@gmail.com

--- submodules/SafariLauncher/build/SafariLauncher.build/Release-iphoneos/SafariLauncher.build/Objects-normal/armv7s/SafariLauncher.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/thomas/Movies/qa-appium/submodules/SafariLauncher/SafariLauncher/SafariLauncher.m \
-  /Users/thomas/Movies/qa-appium/submodules/SafariLauncher/SafariLauncher/SafariLauncher.h \
-  /Users/thomas/Movies/qa-appium/submodules/SafariLauncher/SafariLauncher/Preferences.h
--- submodules/SafariLauncher/build/SafariLauncher.build/Release-iphoneos/SafariLauncher.build/Objects-normal/armv7s/ViewController.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/thomas/Movies/qa-appium/submodules/SafariLauncher/SafariLauncher/ViewController.m \
-  /Users/thomas/Movies/qa-appium/submodules/SafariLauncher/SafariLauncher/ViewController.h \
-  /Users/thomas/Movies/qa-appium/submodules/SafariLauncher/SafariLauncher/Preferences.h \
-  /Users/thomas/Movies/qa-appium/submodules/SafariLauncher/SafariLauncher/SafariLauncher.h
